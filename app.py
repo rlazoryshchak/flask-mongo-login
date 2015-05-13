e5af6a2f7b71fe1c50d47131194c12fb7e125ab5
Roman Lazoryshchak <roman.lazoryshchak@onapp.com>
2015-05-13 17:53:53 +0300

    First commit

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import *
+
+app = Flask(__name__)
+
+app.secret_key = 'pcduino'
+
+@app.route('/')
+def home():
+	return 'Home'
+
+@app.route('/welcome')
+def welcome():
+	return render_template('welcome.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	error = None
+	if request.method == 'POST':
+		if request.form['user'] != 'admin' or request.form['password'] != 'admin':
+			error = 'Invalid credentials'
+		else:
+			session['logged_in'] = True
+			flash('You are logged in')
+			return redirect(url_for('home'))
+	return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+	session.pop('logged_in', None)
+	return redirect(url_for('welcome'))
+
+if __name__ == '__main__':
+	app.run(debug=True)
