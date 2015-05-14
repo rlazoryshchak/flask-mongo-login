a28f254276bbeef803596f216ac1d62f572d9d8f
Roman <pinchlog1@gmail.com>
2015-05-14 22:29:44 +0300

    Add login

--- app.py
+++ app.py
@@ -1,37 +1,48 @@
-from flask import *
+from flask import Flask, flash, redirect, url_for, request, get_flashed_messages, render_template, session
+from flask.ext.login import LoginManager, current_user, login_user, logout_user, login_required
 from models import *
 import rlcompleter, pdb
 
 # pdb.Pdb.complete=rlcompleter.Completer(locals()).complete;pdb.set_trace()
 
 app = Flask(__name__)
-
+login_manager = LoginManager()
+login_manager.init_app(app)
 app.secret_key = 'pcduino'
+
+@login_manager.user_loader
+def load_user(id):
+    return User.get(id)
 
 @app.route('/')
 def home():
-	return 'Home'
+    return 'Home'
+
 
 @app.route('/welcome')
+@login_required
 def welcome():
-	return render_template('welcome.html')
+    return render_template('welcome.html')
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-	error = None
-	if request.method == 'POST':
-		if request.form['user'] != 'admin' or request.form['password'] != 'admin':
-			error = 'Invalid credentials'
-		else:
-			session['logged_in'] = True
-			flash('You are logged in')
-			return redirect(url_for('welcome'))
-	return render_template('login.html', error=error)
+    if request.method == 'POST':
+        user = User.get(request.form['username'])
+        if (user and user.password == request.form['password']):
+            login_user(user)
+            return redirect(url_for('welcome'))
+        else:
+            flash('Username or password incorrect')
+            return render_template('login.html')
+    return render_template('login.html')
+
 
 @app.route('/logout')
 def logout():
-	session.pop('logged_in', None)
-	return redirect(url_for('welcome'))
+    logout_user()
+    return redirect(url_for('home'))
+
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=False)
--- models.py
+++ models.py
@@ -1,7 +1,25 @@
 from mongoengine import *
+from flask.ext.login import UserMixin
 
 connect('pcduino', host='mongodb://pcduino:pcduino@ds031932.mongolab.com:31932/pcduino')
 
-class User(Document):
-    login = StringField(required=True)
-    password = StringField(required=True, max_length=200)
+class User(UserMixin):
+    USERS = {'roman': 'admin'}
+    # login = StringField(required=True)
+    # password = StringField(required=True, max_length=200)
+
+    def __init__(self, id):
+        if not id in self.USERS:
+            raise UserNotFoundError()
+        self.id = id
+        self.password = self.USERS[id]
+
+    @classmethod
+    def get(self_class, id):
+        try:
+            return self_class(id)
+        except UserNotFoundError:
+            return None
+
+class UserNotFoundError(Exception):
+    pass
